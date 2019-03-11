4a7c03ae3e7e8f502f7dce295725e6ec229222bc
Maxouuille <maxime.i@hotmail.com>
2019-03-11 14:24:58 +0100

    .gitignore

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 3000
 
-const hostname = 'nodejsexo1.herokuapp.com';
-const port = 3000;
+app.get('/hello', (req, res) => res.send('Hello World'))
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at https://${hostname}:${port}/hello`);
-});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
