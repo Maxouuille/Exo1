32d29fbd64cae19123b54e3a6788bd05c42025c7
Maxouuille <maxime.i@hotmail.com>
2019-03-11 15:04:10 +0100

    Ajout de la method POST

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const app = express()
-var port = process.env.PORT
 
-app.get('/hello', (req, res) => res.send('Hello World'))
+var port = process.env.PORT || 3000 
+
+app.use(express.json())
+
+app.get('/hello', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.post('/chat', function (req, res) {
+   	if(req.body.msg === "ville"){
+   		console.log("Nous sommes à Paris")
+   	}else if (req.body.msg === "météo") {
+   		console.log("Il fait beau")
+   	}
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
